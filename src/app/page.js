c68c4ee40b61a138e62b4e1af582b05707ca374b
yyyaaassshh <yashpawar2995@gmail.com>
2025-08-05 12:34:55 +0530

    feat: added github repo

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { Github } from "lucide-react";
 import Row1 from "@/components/Row1";
 import Row2 from "@/components/Row2";
 import Row3 from "@/components/Row3";
@@ -9,8 +9,10 @@ import Row6 from "@/components/Row6";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen  select-none  flex items-center justify-center ">
-      {" "}
+    <div className="h-screen w-screen  select-none  flex flex-col items-center justify-center ">
+      <a href="https://github.com/spidey279/keyboard-component" target="_blank" className="p-2  -translate-y-15 hover:bg-[#363636] duration-150 cursor-pointer rounded-full">
+        <Github className="text-black hover:text-white duration-150  w-6 h-6" />
+      </a>{" "}
       <div className="rotate-90 md:rotate-0 origin-center transition-transform duration-300">
         <div className="h-75 w-[42.8rem] max-w-[700px] rounded-xl text-white p-2 bg-zinc-800">
           <div className=" h-full flex flex-col items-center justify-center gap-1.5">
